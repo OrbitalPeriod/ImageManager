5027eb470a1911477a8f3cfc5fbbd47257729778
Thomas de Gruijter <thomasdegruijter@pm.me>
2026-01-15 20:55:27 +0100

    fix: Fixed broken build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,22 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
+  turbopack: {
+    resolveAlias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 };
 
 export default nextConfig;
